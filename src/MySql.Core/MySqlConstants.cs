620f49821e5aa775fae1b83682252511e76352e0
Kevin Obee <kvn@cluedin.com>
2019-05-16 04:25:19 +0100

    MySql.Core\MySqlConstants.cs migrated

--- src/MySql.Core/MySqlConstants.cs
+++ src/MySql.Core/MySqlConstants.cs
@@ -16,29 +16,28 @@ namespace CluedIn.Crawling.MySql.Core
     public const string ProviderRootCodeValue = "MySql";
     public const string CrawlerName = "MySqlCrawler";
     public const string CrawlerComponentName = "MySqlCrawler";
-    public const string CrawlerDescription = "MySql is a ... to be completed ..."; // TODO complete the crawler description
+    public const string CrawlerDescription = "MySql is a relational database";
     public const string CrawlerDisplayName = "MySql";  // TODO RJ - this field is never used can it be removed ?
-    public const string Uri = "http://www.sampleurl.com";
+    public const string Uri = "https://www.mysql.com/";
 
-     
 
-    public static readonly Guid ProviderId = Guid.Parse("e905382e-aa30-45fa-b434-3c9fb60a358f");   // TODO: Replace value
-    public const string ProviderName = "MySql";         // TODO: Replace value
+    public static readonly Guid ProviderId = Guid.Parse("620bbc66-0545-4490-a74d-bf2a2f4299be");   // TODO: Replace value
+    public const string ProviderName = "MySql";
     public const bool SupportsConfiguration = true;             // TODO: Replace value
-    public const bool SupportsWebHooks = false;             // TODO: Replace value
+    public const bool SupportsWebHooks = true;
     public const bool SupportsAutomaticWebhookCreation = true;             // TODO: Replace value
     public const bool RequiresAppInstall = false;            // TODO: Replace value
     public const string AppInstallUrl = null;             // TODO: Replace value
     public const string ReAuthEndpoint = null;             // TODO: Replace value
-    public const string IconUri = "https://s3-eu-west-1.amazonaws.com/staticcluedin/bitbucket.png"; // TODO: Replace value
+        public const string IconUri = "https://s3-eu-west-1.amazonaws.com/staticcluedin/mysql.png"; // TODO: Replace value
 
     public static readonly ComponentEmailDetails ComponentEmailDetails = new ComponentEmailDetails
     {
       Features = new Dictionary<string, string>
             {
-                                       { "Tracking",        "Expenses and Invoices against customers" },
-                                       { "Intelligence",    "Aggregate types of invoices and expenses against customers and companies." }
-                                   },
+                { "Tracking",        "Expenses and Invoices against customers" },
+                { "Intelligence",    "Aggregate types of invoices and expenses against customers and companies." }
+            },
       Icon = new Uri(IconUri),
       ProviderName = ProviderName,
       ProviderId = ProviderId,
