5b0ea74ede6f239dba5e6d6f0366021a94151d01
Kevin Obee <kvn@cluedin.com>
2019-05-16 10:17:18 +0100

    gci . *.csproj -recurse | ForEach-Object { dotnet migrate-2017 migrate $_.FullName }

--- test/integration/Crawling.MySql.Integration.Test/Properties/AssemblyInfo.cs
+++ test/integration/Crawling.MySql.Integration.Test/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Crawling.MySql.Integration.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Crawling.MySql.Integration.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("99f9ea75-a5f3-48cf-87e4-c4b7481787d3")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
